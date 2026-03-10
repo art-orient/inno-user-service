b2d9e74c2b959295310226a8dbf7b2ffeb26f14e
Aliaksandr Artsikhovich <art-orient@tut.by>
2026-03-10 14:49:54 +0300

    Add luquibase scripts and entity

---
+++ src/main/java/by/art/user_service/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package by.art.user_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}

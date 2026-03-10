861939674712159d1df575bdffecbe499b17554b
Aliaksandr Artsikhovich <art-orient@tut.by>
2026-03-11 00:01:45 +0300

    Revert "Add luquibase scripts and entity"
    This reverts commit 06749e7ba151faf5d82e1e7d61e860ff43d266c7.

--- src/main/java/by/art/user_service/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package by.art.user_service.repository;
-
-import by.art.user_service.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}

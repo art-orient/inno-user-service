d47ce11c4ea82e75af0be40a4ed146a5f19780e7
Aliaksandr Artsikhovich <art-orient@tut.by>
2026-03-15 17:17:54 +0300

    add integration tests

---
+++ src/test/java/by/art/user_service/integration/UserIntegrationTest.java
@@ -0,0 +1,94 @@
+package by.art.user_service.integration;
+
+import by.art.user_service.TestContainersConfig;
+import by.art.user_service.dto.UserDto;
+import by.art.user_service.entity.User;
+import by.art.user_service.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserIntegrationTest extends TestContainersConfig {
+  @Autowired
+  private MockMvc mockMvc;
+  @Autowired
+  private UserRepository userRepository;
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  private User createTestUser() {
+    User user = new User();
+    user.setName("Alex");
+    user.setSurname("Artsikhovich");
+    user.setEmail("orientirik@gmail.com");
+    user.setActive(true);
+    return userRepository.save(user);
+  }
+
+  @Test
+  void createUser_success() throws Exception {
+    UserDto dto = new UserDto();
+    dto.setName("Alex");
+    dto.setSurname("Artsikhovich");
+    dto.setEmail("orientirik@gmail.com");
+    dto.setActive(true);
+
+    mockMvc.perform(post("/users")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(dto)))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.id", notNullValue()))
+            .andExpect(jsonPath("$.name", is("Alex")))
+            .andExpect(jsonPath("$.surname", is("Artsikhovich")));
+  }
+
+  @Test
+  void getUser_success() throws Exception {
+    User user = createTestUser();
+    user = userRepository.save(user);
+
+    mockMvc.perform(get("/users/" + user.getId()))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.id", is(user.getId().intValue())))
+            .andExpect(jsonPath("$.email", is("orientirik@gmail.com")));
+  }
+
+  @Test
+  void updateUser_success() throws Exception {
+    User user = createTestUser();
+    user = userRepository.save(user);
+
+    UserDto updateDto = new UserDto();
+    updateDto.setName("Updated");
+    updateDto.setSurname("User");
+    updateDto.setEmail("updated@gmail.com");
+    updateDto.setActive(false);
+    mockMvc.perform(put("/users/" + user.getId())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(updateDto)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.name", is("Updated")))
+            .andExpect(jsonPath("$.active", is(false)));
+  }
+
+  @Test
+  void deleteUser_success() throws Exception {
+    User user = createTestUser();
+    user = userRepository.save(user);
+
+    mockMvc.perform(delete("/users/" + user.getId()))
+            .andExpect(status().isNoContent());
+    assertFalse(userRepository.findById(user.getId()).isPresent());
+  }
+}

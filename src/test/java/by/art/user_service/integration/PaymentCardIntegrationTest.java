3784df79cfaa0bec8b7ae0ba20a1bafe7cf51b65
Aliaksandr Artsikhovich <art-orient@tut.by>
2026-03-17 23:50:23 +0300

    debug test

--- src/test/java/by/art/user_service/integration/PaymentCardIntegrationTest.java
+++ src/test/java/by/art/user_service/integration/PaymentCardIntegrationTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -28,6 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @Testcontainers
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@TestPropertySource(locations = "classpath:application-test.yml")
 class PaymentCardIntegrationTest extends TestcontainersConfig {
   @Autowired
   private MockMvc mockMvc;

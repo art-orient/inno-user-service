b05f05de4db53e69b3d0f70c5cb268187ec5eb18
Aliaksandr Artsikhovich <art-orient@tut.by>
2026-04-30 15:16:45 +0300

    add k8s

--- src/main/java/com/innowise/userservice/config/SecurityConfig.java
+++ src/main/java/com/innowise/userservice/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/actuator/health").permitAll()
                     .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
                     .requestMatchers(HttpMethod.DELETE, "/api/users/*")
                     .access((authentication, ctx) -> {
